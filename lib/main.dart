bcfbb611210a409b0c4ad70701c44e92f1465cda
GaleanoJorge <juandavidrianoperez@gmail.com>
2023-03-30 18:20:25 -0500

    authentifiction with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uber_clone/src/pages/home/home_page.dart';
 import 'package:uber_clone/src/pages/login/login_page.dart';
 import 'package:uber_clone/src/utils/Colors.dart' as utils;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:uber_clone/src/providers/auth_provider.dart';
 
 class LoginController {
-
   BuildContext? context;
 
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
+  AuthProvider? _authProvider;
+
   Future? init(BuildContext context) {
     this.context = context;
+
+    _authProvider = new AuthProvider();
   }
 
-  void login() {
-    String email = emailController.text;
-    String password = passwordController.text;
+  void login() async {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
 
     print('Email: $email');
     print('Password: $password');
+
+    try {
+      bool isLogin = await _authProvider!.login(email, password);
+
+      if (isLogin) {
+        print('yes');
+      } else {
+        print('no');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
   }
 }

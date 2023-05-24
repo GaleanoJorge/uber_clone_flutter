b201ebf6c151c71a8d6f87c75615ef3f19097177
Jorge Galeano <jgaleano@kinpos.com>
2023-05-24 11:46:13 -0500

    rotacion del icono del mapa segun el desplazamiento

--- lib/src/pages/driver/driver_map/driver_map_controller.dart
+++ lib/src/pages/driver/driver_map/driver_map_controller.dart
@@ -149,10 +149,16 @@ class DriverMapController {
       String content, BitmapDescriptor iconMarker) {
     MarkerId id = MarkerId(markerId);
     Marker marker = Marker(
-        markerId: id,
-        icon: iconMarker,
-        position: LatLng(lat, lng),
-        infoWindow: InfoWindow(title: title, snippet: content));
+      markerId: id,
+      icon: iconMarker,
+      position: LatLng(lat, lng),
+      infoWindow: InfoWindow(title: title, snippet: content),
+      draggable: false,
+      zIndex: 2,
+      flat: true,
+      anchor: Offset(0.5, 0.5),
+      rotation: _position.heading,
+    );
 
     markers[id] = marker;
   }
